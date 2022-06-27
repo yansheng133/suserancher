095bbff430c7cae4cd66465b7f5942a51f755345
YanSheng <sam012822@gmail.com>
2022-06-27 15:50:01 +0800

    add basic init script

---
+++ rke2-initNode.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+curl -sfL https://get.rke2.io | sh -
+export PATH=$PATH:/opt/rke2/bin
+
+mkdir -p /etc/rancher/rke2/
+touch /etc/rancher/rke2/config.yaml
+cat <<EOF | sudo tee /etc/rancher/rke2/config.yaml
+node-name:
+  - "rke2-1"
+token: my-shared-secret
+EOF
+systemctl enable --now rke2-server.service
+
+curl -LO https://dl.k8s.io/release/v1.23.7/bin/linux/amd64/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+mkdir -p /home/rancher/.kube/
+sudo cp /etc/rancher/rke2/rke2.yaml /home/rancher/.kube/config
+sudo chown rancher -v .kube/config
+echo 'source <(kubectl completion bash)' >> /home/rancher/.bashrc
+source /home/rancher/.bashrc
