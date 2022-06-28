287d2bad1d23ee1e0a4e7360f3aefdc8faaa8366
yansheng133 <sam012822@gmail.com>
2022-06-28 09:51:04 +0800

    down grade cert-manager

--- initRancher.sh
+++ initRancher.sh
@@ -7,12 +7,12 @@ sudo mv linux-amd64/helm /usr/local/bin/helm
 kubectl create ns cert-manager 
 kubectl create namespace cattle-system
 
-kubectl apply --validate=false -f https://github.com/cert-manager/cert-manager/releases/download/v1.8.1/cert-manager.crds.yaml
+kubectl apply --validate=false -f https://github.com/cert-manager/cert-manager/releases/download/v1.7.1/cert-manager.crds.yaml
 
 helm repo add jetstack https://charts.jetstack.io
 helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
 helm repo update
 
-helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --version v1.8.1
+helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --version v1.7.1
 
 helm install rancher rancher-stable/rancher --namespace cattle-system --set hostname=rancher.example.com --set replicas=3
